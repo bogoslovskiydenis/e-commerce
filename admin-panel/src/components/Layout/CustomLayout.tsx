147042dc5b22d23c632529b97f2382d5cf4468d0
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2025-06-25 15:56:35 +0300

    убрал ненужное , почистил проект

--- admin-panel/src/components/Layout/CustomLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Layout } from 'react-admin';
-import { CustomSidebar } from './CustomSidebar';
-
-export const CustomLayout = (props: any) => (
-    <Layout {...props} sidebar={CustomSidebar} />
-);
--- admin-panel/src/components/Layout/CustomSidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Sidebar } from 'react-admin';
-import { CustomMenu } from './CustomMenu';
-
-export const CustomSidebar = (props: any) => (
-    <Sidebar {...props}>
-        <CustomMenu />
-    </Sidebar>
-);
